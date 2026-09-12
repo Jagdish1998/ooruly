f35c30784f9c5850a3eb6b185266785bc8ffbdb4
Jagdish Sahoo <jagdishsahoo9@gmail.com>
2026-09-12 19:13:50 +0530

    fix: fixed mobile issues

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  *
  * Bump VERSION whenever the shell list changes to force a clean re-cache.
  */
-const VERSION = 'ooruly-v13';
+const VERSION = 'ooruly-v14';
 const SHELL = [
     './',
     './index.html',
